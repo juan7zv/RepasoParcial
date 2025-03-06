9989fee4c9c1ce7e4acf1ae82ec7f24d01d53d32
Carlos Patino <carlosmariopatinoraamirez@gmail.com>
2025-03-06 17:55:57 -0500

    Agregado Metodo C

--- Inventario.java
+++ Inventario.java
@@ -4,5 +4,8 @@ public class Inventario{
     }
     public void metodoB() {
         System.out.println("Ejecutando método B");
-        }
+    }
+    public void metodoC(){
+        System.out.println("Ejecutando método C");
+    }
 }
