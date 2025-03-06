fdac83de635bd3d0ecdf7f8a650c230f17d6ebb5
Juan Diego Velasco <velasco.juan.7583@eam.edu.co>
2025-03-06 17:51:49 -0500

    Agregado metodo B

--- Inventario.java
+++ Inventario.java
@@ -2,4 +2,7 @@ public class Inventario{
     public void metodoA() {
         System.out.println("Ejecutando método A");
     }
+    public void metodoB() {
+        System.out.println("Ejecutando método B");
+        }
 }
